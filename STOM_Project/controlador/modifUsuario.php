df1172c3e72f9d2293fd0e3f4842c4b790ff029a
Tavovizard <49847596+Tavovizard@users.noreply.github.com>
2020-11-26 02:30:54 -0600

    Add files via upload

--- STOM_Project/controlador/modifUsuario.php
+++ STOM_Project/controlador/modifUsuario.php
@@ -2,25 +2,27 @@
 	require_once("../controlador/conexion.php");
 	require_once("../controlador/controlador.php");
 
-function agregaUsuario( $params ){
-            $error = "";
-            $valor = "";
-            $id = $params["idusuario"];
-            $nombre = $params["nombre"];
-            $telefono = $params["telefono"];
-            $correo = $params["correo"];
-            
-            $query = "UPDATE usuarios SET nombre='$nombre', telefono='$telefono', correo='$correo' WHERE idusuario='$id'";
-            
-            
-                
-                if(!empty( $nombre) && !empty( $telefono) && !empty( $correo)) {
-                    if(! $this->conn->query($query)){
-                        $error = 'Ocurrio un error ejecutando el query [' . $this->conn->error . ']';
+
+
+$params = array(
+    "nombre" => $_POST['nombre'],
+    "contrasena" => $_POST['contrasena'],
+    "telefono" => $_POST['telefono'],
+    "correo" => $_POST['correo'],
+);
+   
+            $db = Database::getInstance();
+            $conn = $db->getConnection();
+            $sesion = new Modelo($conn);
+
+            list ($valor,$error) = $sesion->modifUsuario( $params );
+            if ( empty( $valor)){
+                if ( !empty($error)) $_SESSION["error"] = $error;
+                    }else{
+                echo "<script>alert('Se han guardado los cambios')
+                        window.location.href='../vista/perfil.php';
+                                </script>";
                     }
-                    $valor = $this->conn->affected_rows;
-                }
-            $resul[] = $valor;
-            $resul[] = $error;
-            return $resul;
-        }
+       
+
+?>
